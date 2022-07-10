be3e948d67fcdb1668d86ee94954ac6cdbdefe5c
karthikn130 <karthikn130@gmail.com>
2022-07-10 06:39:07 +0530

    vina parser added

---
+++ vina_parser.py
@@ -0,0 +1,26 @@
+
+# parse a text file with vina output
+
+import os
+
+
+def vina_score(vina_log):
+    search = "   1"
+    ligand_name = "Output will be"
+    vina_log_file = open(vina_log, "r")
+    lines = vina_log_file.readlines()
+    for line in lines:
+        if line.startswith(ligand_name):
+            word = line.split()
+            ligand = word[3]
+    
+        if line.startswith(search):
+            word = line.split()
+            score = word[1]
+            print(score, ligand)
+            
+        
+
+vina_score("vina_log.txt")
+
+
