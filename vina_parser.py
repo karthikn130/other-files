3d7529bc88f3815a3a4b7c9d96feb827392b4d7c
karthikn130 <karthikn130@gmail.com>
2022-07-13 21:25:16 +0530

    removed some files

--- vina_parser.py
+++ vina_parser.py
@@ -4,24 +4,8 @@
 import os
 
 
-def vina_score(vina_log):
-    search = "   1"
-    ligand_name = "Output will be"
-    vina_log_file = open(vina_log, "r")
-    lines = vina_log_file.readlines()
-    for line in lines:
-        if line.startswith(ligand_name):
-            word = line.split()
-            ligand = word[3]
-    
-        if line.startswith(search):
-            word = line.split()
-            score = word[1]
-            print(score, ligand)
-            
-        
+from ..dlg_parser_pymol_plugin import parsers
 
+#parsers.parse_vina("vina_log.txt")
 
-if __name__=="__main__":
-    vina_score("vina_log.txt")
 
